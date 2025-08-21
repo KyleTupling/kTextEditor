0ad28f482e2c10da1a3fc643fa3e27c7a474ebcc
Kyle <kyletupling@hotmail.com>
2025-08-21 17:41:39 +0100

    Horizontal scrolling, auto-vertical scroll, kEvent function signature

--- src/editor.c
+++ src/editor.c
@@ -6,6 +6,76 @@
 
 #include "theme.h"
 
+static void set_current_font(Editor* e, const char* fontname, int fontsize)
+{
+    size_t len = strlen("resources/fonts/") + strlen(fontname) + 1;
+    char* font_path = malloc(len);
+    if (font_path)
+    {
+        snprintf(font_path, len, "resources/fonts/%s", fontname);
+        e->current_font = font_manager_get_font(font_path, fontsize);
+        e->line_height = TTF_FontLineSkip(e->current_font);
+        free(font_path);
+    }
+}
+
+static void editor_clamp_scroll_y(Editor* e)
+{
+    int content_height = e->num_lines * e->line_height + 25; // 25 = infobar height
+    int max_scroll = content_height - (e->viewport_height + 25) + (e->line_height * e->cursor_margin_lines_y);
+    if (max_scroll < 0) max_scroll = 0;
+    if (e->scroll_offset_y > max_scroll) e->scroll_offset_y = max_scroll;
+}
+
+static void move_cursor(Editor* e, int new_line, int new_col, bool shift)
+{
+    if (shift)
+    {
+        if (!e->is_selecting)
+        {
+            e->selection_start_line = e->cursor_line;
+            e->selection_start_col  = e->cursor_col;
+            e->is_selecting = true;
+        }
+
+        e->selection_end_line = new_line;
+        e->selection_end_col  = new_col;
+    }
+    else
+    {
+        e->is_selecting = false;
+    }
+
+    e->cursor_line = new_line;
+    e->cursor_col  = new_col;
+    e->cursor_cooldown = 1.0f;
+
+    // infobar height = 25px
+    int usable_height = e->viewport_height - 25; //TODO: Update this when infobar is refactored
+    int first_visible_line = e->scroll_offset_y / e->line_height;
+    int last_visible_line = (e->scroll_offset_y + usable_height) / e->line_height;
+
+    // Ensuring that cursor is always visible on the viewport
+    // A number of editor lines around the cursor are set to be always visible (Editor::cursor_margin_lines_y)
+    if (e->cursor_line < first_visible_line + e->cursor_margin_lines_y)
+    {
+        if (e->cursor_line - e->cursor_margin_lines_y >= 0)
+        {
+            e->scroll_offset_y = (e->cursor_line - e->cursor_margin_lines_y) * e->line_height;
+        }
+        else // If cursor - margin is not greater than or equal to zero, the cursor must be on
+             // line number (cursor_margin_lines_y) at most
+        {
+            e->scroll_offset_y = 0;
+        }
+    }
+    else if (e->cursor_line > last_visible_line - e->cursor_margin_lines_y)
+    {
+        int lines_fit = usable_height / e->line_height;
+        e->scroll_offset_y = (e->cursor_line - lines_fit + e->cursor_margin_lines_y) * e->line_height;
+    }
+}
+
 void editor_init(Editor* e)
 {
     // Empty editor and original copy
@@ -17,6 +87,7 @@ void editor_init(Editor* e)
 
     e->num_lines = 1;
 
+    e->scroll_offset_x = 0;
     e->scroll_offset_y = 0;
 
     e->cursor_line = 0;
@@ -26,6 +97,8 @@ void editor_init(Editor* e)
     e->cursor_blink_duration = 1.2f;
     e->cursor_cooldown = 1.0f;
 
+    e->cursor_margin_lines_y = 3;
+
     e->is_selecting = false;
     e->selection_start_line = 0;
     e->selection_start_col = 0;
@@ -34,12 +107,13 @@ void editor_init(Editor* e)
     
     e->text_changed = false;
     e->is_saved = true;
-    e->needs_save_check = false;
 
     set_current_filename(e, "");
 
-    e->current_font = font_manager_get_font("resources/fonts/SourceCodePro-Bold.ttf", 20);
+    //e->current_font = font_manager_get_font("resources/fonts/SourceCodePro-Bold.ttf", 20);
+    set_current_font(e, "SourceCodePro-Bold.ttf", 18);
 
+    e->viewport_width = 0;
     e->viewport_height = 0;
 
     printf("[editor] Editor initialised.\n");
@@ -244,108 +318,88 @@ void editor_handle_key(Editor* e, kKeycode key, kKeymod mod)
                 }
                 strcpy(e->lines[e->cursor_line + 1], &line[e->cursor_col]);
                 line[e->cursor_col] = '\0';
-                e->cursor_line++;
-                e->cursor_col = 0;
+                move_cursor(e, e->cursor_line + 1, 0, false);
                 e->num_lines++;
             }
             break;
 
-        case KKEY_LEFT: // LEFT
-            if (mod == KKEYMOD_SHIFT)
+        case KKEY_LEFT:
+        {
+            int new_line = e->cursor_line;
+            int new_col  = e->cursor_col;
+
+            if (new_col > 0) new_col--;
+            else if (new_line > 0) 
             {
-                if (!e->is_selecting)
-                {
-                    e->selection_start_line = e->cursor_line;
-                    e->selection_start_col  = e->cursor_col;
-                    e->is_selecting = true;
-                }
-                
-                if (e->cursor_col > 0) e->cursor_col--;
-                else if (e->cursor_line > 0) 
-                {
-                    e->cursor_line--;
-                    e->cursor_col = strlen(e->lines[e->cursor_line]);
-                }
-                e->selection_end_line = e->cursor_line;
-                e->selection_end_col = e->cursor_col;
-            }
-            else
-            {
-                if (e->cursor_col > 0) e->cursor_col--;
-                else if (e->cursor_line > 0) 
-                {
-                    e->cursor_line--;
-                    e->cursor_col = strlen(e->lines[e->cursor_line]);
-                }
-                e->is_selecting = false;
+                new_line--;
+                new_col = strlen(e->lines[new_line]);
             }
 
-            e->cursor_cooldown = 1.0f;
+            move_cursor(e, new_line, new_col, mod == KKEYMOD_SHIFT);
             break;
+        }
 
-        case KKEY_RIGHT: // RIGHT
-            // If shift is held, start or continue text selection
-            // Otherwise, move cursor and disable text selection
-            if (mod == KKEYMOD_SHIFT)
+        case KKEY_RIGHT:
+        {
+            int new_line = e->cursor_line;
+            int new_col  = e->cursor_col;
+            int line_len = strlen(e->lines[new_line]);
+
+            if (new_col < line_len) new_col++;
+            else if (new_line + 1 < e->num_lines) 
             {
-                if (!e->is_selecting)
-                {
-                    e->selection_start_line = e->cursor_line;
-                    e->selection_start_col  = e->cursor_col;
-                    e->is_selecting = true;
-                }
-
-                if (e->cursor_col < strlen(line)) e->cursor_col++;
-                else if (e->cursor_line + 1 < e->num_lines) 
-                {
-                    e->cursor_line++;
-                    e->cursor_col = 0;
-                }
-
-                e->selection_end_line = e->cursor_line;
-                e->selection_end_col = e->cursor_col;
+                new_line++;
+                new_col = 0;
             }
-            else
-            {
-                if (e->cursor_col < strlen(line)) e->cursor_col++;
-                else if (e->cursor_line + 1 < e->num_lines) 
-                {
-                    e->cursor_line++;
-                    e->cursor_col = 0;
-                }
-                e->is_selecting = false;
-            }
-            
-            e->cursor_cooldown = 1.0f;
+
+            move_cursor(e, new_line, new_col, mod == KKEYMOD_SHIFT);
             break;
+        }
 
-        case KKEY_UP: // UP
-            if (!mod == KKEYMOD_SHIFT)
+        case KKEY_UP:
+        {
+            if (e->cursor_line > 0)
+            {
+                int new_line = e->cursor_line - 1;
+                int new_col  = e->cursor_col;
+                int line_len = strlen(e->lines[new_line]);
+                if (new_col > line_len) new_col = line_len;
+
+                move_cursor(e, new_line, new_col, mod == KKEYMOD_SHIFT);
+            }
+            else if (!(mod == KKEYMOD_SHIFT))
             {
                 e->is_selecting = false;
             }
-
-            if (e->cursor_line > 0) 
-            {
-                e->cursor_line--;
-                if (e->cursor_col > strlen(e->lines[e->cursor_line]))
-                    e->cursor_col = strlen(e->lines[e->cursor_line]);
-            }
-            e->cursor_cooldown = 1.0f;
             break;
+        }
 
-        case KKEY_DOWN: // DOWN
-            if (e->cursor_line + 1 < e->num_lines) 
+        case KKEY_DOWN:
+        {
+            if (e->cursor_line + 1 < e->num_lines)
             {
-                e->cursor_line++;
-                if (e->cursor_col > strlen(e->lines[e->cursor_line]))
-                    e->cursor_col = strlen(e->lines[e->cursor_line]);
+                int new_line = e->cursor_line + 1;
+                int new_col  = e->cursor_col;
+                int line_len = strlen(e->lines[new_line]);
+                if (new_col > line_len) new_col = line_len;
+
+                move_cursor(e, new_line, new_col, mod == KKEYMOD_SHIFT);
             }
-            e->cursor_cooldown = 1.0f;
+            else if (!(mod == KKEYMOD_SHIFT))
+            {
+                e->is_selecting = false;
+            }
             break;
+        }
 
         case KKEY_F1: // F1
-            e->current_font = font_manager_get_font(font_manager_get_font_path(e->current_font), font_manager_get_font_size(e->current_font) + 1);
+            set_current_font(e, "SourceCodePro-Bold.ttf", font_manager_get_font_size(e->current_font) - 1);
+            editor_clamp_scroll_y(e);
+            break;
+
+        case KKEY_F2:
+            set_current_font(e, "SourceCodePro-Bold.ttf", font_manager_get_font_size(e->current_font) + 1);
+            editor_clamp_scroll_y(e);
             break;
 
         case KKEY_F5: // F5
@@ -356,7 +410,7 @@ void editor_handle_key(Editor* e, kKeycode key, kKeymod mod)
     }
 }
 
-void editor_handle_mouse_down(Editor* e, kMouseButton btn)
+void editor_handle_mouse_down(Editor* e, kMouseButtonEvent btn)
 {
     // if (btn == K_MOUSE_BUTTON_LEFT)
     // {
@@ -364,16 +418,48 @@ void editor_handle_mouse_down(Editor* e, kMouseButton btn)
     // }
 }
 
-void editor_handle_scroll_y(Editor* e, Sint32 wheel_y)
+void editor_handle_scroll(Editor* e, kMouseWheelEvent wheel)
 {
-    e->scroll_offset_y -= wheel_y * e->line_height;
+    if (wheel.mod == KKEYMOD_SHIFT)
+    {
+        // Horizontal scroll
+        int first_visible_line = e->scroll_offset_y / e->line_height;
+        int last_visible_line = (e->scroll_offset_y + e->viewport_height) / e->line_height + 1;
 
-    if (e->scroll_offset_y < 0) e->scroll_offset_y = 0;
+        // Find line of text with longest width
+        // Only check lines currently visible
+        int max_width = 0;
+        for (int i = first_visible_line; i < last_visible_line; i++)
+        {
+            int current_line_width; 
+            TTF_SizeText(e->current_font, e->lines[i], &current_line_width, NULL);
+            if (current_line_width > max_width) max_width = current_line_width;
+        }
 
-    int content_height = e->num_lines * e->line_height;
-    int max_scroll = content_height - e->viewport_height + 10;
-    if (max_scroll < 0) max_scroll = 0;
-    if (e->scroll_offset_y > max_scroll) e->scroll_offset_y = max_scroll;
+        if (max_width > e->viewport_width - e->left_margin - 40)
+        {
+            e->scroll_offset_x -= wheel.y * e->line_height * 2;
+            if (e->scroll_offset_x < 0) e->scroll_offset_x = 0;
+            if (e->scroll_offset_x > e->viewport_width + e->left_margin)
+            {
+                e->scroll_offset_x = e->viewport_width + e->left_margin;
+            }
+        }
+        else
+        {
+            e->scroll_offset_x = 0;
+        }
+    }
+    else
+    {
+        // Vertical Scroll
+        e->scroll_offset_y -= wheel.y * e->line_height;
+
+        //TODO: Move this to clamp scroll function
+        if (e->scroll_offset_y < 0) e->scroll_offset_y = 0;
+
+        editor_clamp_scroll_y(e);
+    }    
 }
 
 void editor_render(Editor* e, struct Renderer* r)
@@ -381,7 +467,7 @@ void editor_render(Editor* e, struct Renderer* r)
     SDL_Color textColor = { 230, 230, 230, 255 };
     SDL_Color lineNumberColor = { 100, 180, 100, 255};
 
-    e->line_height = TTF_FontLineSkip(e->current_font);
+    //e->line_height = TTF_FontLineSkip(e->current_font);
 
     // Determine width of line numbers (in pixels) for the current font
     int max_line_number = e->num_lines;
@@ -403,18 +489,9 @@ void editor_render(Editor* e, struct Renderer* r)
     SDL_Color lh_color = {25, 25, 25, 255};
     renderer_draw_rect(r, lh.x, lh.y, lh.w, lh.h, lh_color);
 
-    
-
     for (size_t i = first_visible_line; i < last_visible_line; i++)
     {
         int y = (i * e->line_height) - e->scroll_offset_y;
-
-        char lineNumber[16];
-        snprintf(lineNumber, sizeof(lineNumber), "%4d", i + 1);
-        if (i == e->cursor_line) lineNumberColor.a = 255;
-        else lineNumberColor.a = 100;
-        
-        renderer_draw_text(r, lineNumber, line_number_width + 20, y, e->current_font, ALIGN_RIGHT, lineNumberColor);
 
         if (e->is_selecting)
         {
@@ -469,7 +546,7 @@ void editor_render(Editor* e, struct Renderer* r)
             }
         }
         
-        renderer_draw_text(r, e->lines[i], e->left_margin + line_number_width + gutter_padding, y, e->current_font, ALIGN_LEFT, textColor);
+        renderer_draw_text(r, e->lines[i], e->left_margin + line_number_width + gutter_padding - e->scroll_offset_x, y, e->current_font, ALIGN_LEFT, textColor);
     }
 
     int cursor_x = 0;
@@ -483,8 +560,26 @@ void editor_render(Editor* e, struct Renderer* r)
         TTF_SizeText(e->current_font, buffer, &cursor_x, NULL);
     }
     int cursor_y = e->cursor_line * e->line_height;
-    renderer_draw_cursor(r, e->left_margin + line_number_width + gutter_padding + cursor_x, e->cursor_line * e->line_height - e->scroll_offset_y, e->line_height, e->cursor_alpha);
+    renderer_draw_cursor(r, e->left_margin + line_number_width + gutter_padding + cursor_x - e->scroll_offset_x, e->cursor_line * e->line_height - e->scroll_offset_y, e->line_height, e->cursor_alpha);
 
+    // Rect behind line numbers
+    // TODO: Remove this extra render call, use logic to NOT draw some text under?
+    SDL_Color bg = {20, 20, 20, 255};
+    renderer_draw_rect(r, 0, 0, e->left_margin + line_number_width - gutter_padding, vp.h, bg);
+
+    for (size_t i = first_visible_line; i < last_visible_line; i++)
+    {
+        int y = (i * e->line_height) - e->scroll_offset_y;
+
+        char lineNumber[16];
+        snprintf(lineNumber, sizeof(lineNumber), "%4d", i + 1);
+        if (i == e->cursor_line) lineNumberColor.a = 255;
+        else lineNumberColor.a = 100;
+
+        
+        renderer_draw_text(r, lineNumber, line_number_width + 20, y, e->current_font, ALIGN_RIGHT, lineNumberColor);
+    }
+    
     char info[128];
     snprintf(info, sizeof(info), "%s%s | Line %d, Col %d", e->current_file, e->is_saved ? "" : "*", e->cursor_line + 1, e->cursor_col + 1);
     renderer_draw_infobar(r, info);
